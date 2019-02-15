97a4bbc785ca4dcc516c5024bda5a809bea58d24
Bart Kastermans <kasterma@kasterma.net>
2019-02-15 09:31:10 +0100

    feat: cleaned up prob3 solution

--- cryptopals/set1/prob3.py
+++ cryptopals/set1/prob3.py
@@ -14,27 +14,18 @@
 # You now have our permission to make "ETAOIN SHRDLU" jokes on Twitter.
 
 import json
-import sys
+
+from set1.utils import apply_key, printable, eng_freq, get_freqtable, dict_dist
 
 cypher1 = bytes.fromhex("1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736")
 
 
-def attempt(cypher_bytes, key_int):
-    return bytes(b ^ key_int for b in cypher_bytes)
-
-
-def printable(i):
-    """Is the integer i printable as ascii"""
-    lower = int.from_bytes(bytes.fromhex('20'), sys.byteorder)
-    upper = int.from_bytes(bytes.fromhex('7e'), sys.byteorder)
-    return lower <= i <= upper
-
-
+# First attempt: look for a message that has all its characters printable
 def search(cypher_bytes):
     """Assume the message is of only printable characters"""
     possibles = {}
     for i in range(256):
-        a = attempt(cypher_bytes, i)
+        a = apply_key(cypher_bytes, i)
         p = [printable(i) for i in a]
         if all(p):
             possibles[str(i)] = a
@@ -47,82 +38,15 @@ possibles = search(cypher1)
 print(json.dumps({k: v.decode('ascii') for k, v in possibles.items()}, indent=2))
 
 
-def dict_dist(d1: dict, d2: dict) -> int:
-    """Distance between two dictionaries"""
-    keys = set(d1.keys()).union(set(d2.keys()))
-    d = 0
-    for key in keys:
-        d += abs(d1.get(key, 20) - d2.get(key, 20))  # 20 is penalty for difference in keys
-        # this penalty makes sense since 20 is a large number compared to the length of the strings
-    return d
-
-
-eng_freq = {
-    "a": 8.167,
-    "b": 1.492,
-    "c": 2.782,
-    "d": 4.253,
-    "e": 12.702,
-    "f": 2.228,
-    "g": 2.015,
-    "h": 6.094,
-    "i": 6.966,
-    "j": 0.153,
-    "k": 0.772,
-    "l": 4.025,
-    "m": 2.406,
-    "n": 6.749,
-    "o": 7.507,
-    "p": 1.929,
-    "q": 0.095,
-    "r": 5.987,
-    "s": 6.327,
-    "t": 9.056,
-    "u": 2.758,
-    "v": 0.978,
-    "w": 2.360,
-    "x": 0.150,
-    "y": 1.974,
-    "z": 0.074}
-
+# Second attempt; use the distribution of letters in English to find the message that is closest to it
 target_freq = {c: v/100 * len(cypher1) for c, v in eng_freq.items()}
 
-decodes = {i: attempt(cypher1, i) for i in range(255)}
+decodes = {i: apply_key(cypher1, i) for i in range(255)}
 
-import pandas as pd
-pd.Series(list(decodes[0].decode('utf-8'))).value_counts()
-
-from collections import Counter
-
-cts = {k: Counter(v.decode('utf-8', errors='ignore'))
-       for k, v in decodes.items()
-       if len(v) == len(v.decode('utf-8', errors='ignore'))}
+cts = {k: get_freqtable(v) for k, v in decodes.items()}
 
 dists = {k: dict_dist(target_freq, dict(ct)) for k, ct in cts.items()}
 
 best = {k: decodes[k].decode('utf-8') for k, ct in dists.items() if ct == min(dists.values())}
 
-# silly implementation of dictionary distance above
-from itertools import chain
-d1 = {1:2, 3: 4}
-d2 = {3: 5, 6: 7}
-
-list(chain(d1.items(), d2.items()))
-
-from collections import defaultdict
-from functools import reduce
-
-def red_f(a, b):
-    a[b[0]].append(b[1])
-    return a
-
-reduce(red_f, chain(d1.items(), d2.items()), defaultdict(list))
-
-
-def red_f(a, b):
-    a[b[0]].append(b[1])
-    return a
-
-reduce(red_f, chain(d1.items(), d2.items()), defaultdict(list)).items()
-reduce(lambda a, b: a + (20 if len(b[1]) == 1 else abs(b[1][0] - b[1][1])),
-       reduce(red_f, chain(d1.items(), d2.items()), defaultdict(list)).items(), 0)
+print(best)
--- cryptopals/set1/utils.py
+++ cryptopals/set1/utils.py
@@ -1,3 +1,7 @@
+import sys
+from collections import Counter
+
+
 def apply_key(bs: bytes, key: int) -> bytes:
     """Apply the key to every byte in bs.
 
@@ -5,3 +9,64 @@ def apply_key(bs: bytes, key: int) -> bytes:
     @:param key key to use
     """
     return bytes(b ^ key for b in bs)
+
+
+def printable(i):
+    """Is the integer i printable as ascii"""
+    lower = int.from_bytes(bytes.fromhex('20'), sys.byteorder)
+    upper = int.from_bytes(bytes.fromhex('7e'), sys.byteorder)
+    return lower <= i <= upper
+
+
+eng_freq = {
+    "a": 8.167,
+    "b": 1.492,
+    "c": 2.782,
+    "d": 4.253,
+    "e": 12.702,
+    "f": 2.228,
+    "g": 2.015,
+    "h": 6.094,
+    "i": 6.966,
+    "j": 0.153,
+    "k": 0.772,
+    "l": 4.025,
+    "m": 2.406,
+    "n": 6.749,
+    "o": 7.507,
+    "p": 1.929,
+    "q": 0.095,
+    "r": 5.987,
+    "s": 6.327,
+    "t": 9.056,
+    "u": 2.758,
+    "v": 0.978,
+    "w": 2.360,
+    "x": 0.150,
+    "y": 1.974,
+    "z": 0.074}
+
+
+def get_freqtable(bs: bytes):
+    """Get a frequency table assuming the bytes are utf-8 encoded.
+
+    If the utf-8 decode fails, return empty table.
+    We decided on this implementation to be easily able to compare with the frequency table of English.
+    """
+    # noinspection PyBroadException
+    try:
+        return Counter(bs.decode('utf-8'))
+    except:
+        return Counter()
+
+
+def dict_dist(d1: dict, d2: dict, miss_costs: int = 20) -> int:
+    """Distance between two dictionaries.
+
+    The distance is the sum of absulte values of differences, but miss_costs * number of keys not in both.
+    """
+    keys = set(d1.keys()).union(set(d2.keys()))
+    d = 0
+    for key in keys:
+        d += abs(d1.get(key, miss_costs) - d2.get(key, miss_costs))
+    return d
